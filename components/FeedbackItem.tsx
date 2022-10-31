39b07078a574d071004a7f4f5ea4872406dbea11
KrisJones <kris.jones@live.co.uk>
2022-10-31 11:09:59 +0000

    2 files modified

--- components/FeedbackItem.tsx
+++ components/FeedbackItem.tsx
@@ -2,10 +2,25 @@ import * as React from 'react';
 import '../style.css';
 import Card from './shared/Card';
 
+import { FaTimes, FaEdit } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+import FeedbackContext from '../context/FeedbackContext';
+
 export default function FeedbackItem({ item }) {
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext);
+
   return (
     <Card>
-      <div className="num-display"> {item.rating} </div>
+      <div className="title-display"> {item.title} </div>
+      <div className="mood-display"> {item.mood} </div>
+
+      <button onClick={() => deleteFeedback(item.id)} className="close">
+        <FaTimes color="purple" />
+      </button>
+      <button onClick={() => editFeedback(item)} className="edit">
+        <FaEdit color="purple" />
+      </button>
+
       <div className="text-display"> {item.text} </div>
     </Card>
   );
